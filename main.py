46d334a0f838ee5e2fc7e056094c7a18207a58b8
Rodabaugh <erik@erikrodabaugh.com>
2024-08-28 08:50:21 -0700

    Updated to actually handle dropped packets.

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ log_file = open(log_name,"w")
 
 log_file.write("Time Sent,Latency,TTL,Dropped\n")
 
-for x in range(20):
+for x in range(3):
     ping = tester.ping("1.1.1.1")
-    packets = tester.parse(ping[0], ping[1])
+    packets = tester.parse(ping[0], ping[1], ping[2])
     for packet in packets:
         print(packet)
         local_time = datetime.datetime.fromtimestamp(packet.time_packet_sent).strftime('%Y-%m-%d %H:%M:%S')
--- ping.py
+++ ping.py
@@ -8,45 +8,50 @@ class Tester():
 
     def ping(self, host, packets = 1):
         time_packet_sent = time.time()
-        responce = subprocess.check_output(["ping", self.ping_count_flag, "1", host])
-        return responce.decode("utf-8").lower(), time_packet_sent
+        responce = subprocess.run(["ping", self.ping_count_flag, "1", host], stdout = subprocess.PIPE, check=False)
+        return responce.stdout.decode("utf-8").lower(), responce.returncode, time_packet_sent
 
-    def parse(self, raw_data, time_packet_sent):
+    def parse(self, raw_data, returncode, time_packet_sent):
         # system time, ttl, time
-        if platform.system().lower() == "windows":
-            split_lines = raw_data.split("\r\n")
+        packets = list()
+
+        if returncode == 1:
+            packets.append(packet(None, None, time_packet_sent, dropped=True))
+            return packets
         else:
-            split_lines = raw_data.split("\n")
-        del split_lines[0]
-        if platform.system().lower() == "windows":
+            if platform.system().lower() == "windows":
+                split_lines = raw_data.split("\r\n")
+            else:
+                split_lines = raw_data.split("\n")
             del split_lines[0]
-        clean_lines = []
-        for line in split_lines:
-            if not line == '':
-                clean_lines.append(line)
-            if "ping statistics" in line:
-                break
-                
-            else:
-                break 
-        
-        for line in clean_lines:
-            sections = line.split(" ")
-            packets = list()
-            ttl = None
-            latency = None
-            for section in sections:
-                if "time=" in section:
-                    latency = section.strip("time=")
-                if "ttl" in section:
-                    ttl = section.strip("ttl=")
-            if ttl == None:
-                packets.append(packet(dropped = True))
-            else:
-                packets.append(packet(latency, ttl, time_packet_sent))
-        return packets
+            if platform.system().lower() == "windows":
+                del split_lines[0]
+            clean_lines = []
+
+            for line in split_lines:
+                print(line)
+                if not line == '':
+                    clean_lines.append(line)
+                if "ping statistics" in line:
+                    break
+                    
+                else:
+                    break 
             
-            
+            for line in clean_lines:
+                sections = line.split(" ")
+                ttl = None
+                latency = None
+                for section in sections:
+                    if "time=" in section:
+                        latency = section.strip("time=")
+                    if "ttl" in section:
+                        ttl = section.strip("ttl=")
+                if ttl == None:
+                    packets.append(packet(dropped = True))
+                else:
+                    packets.append(packet(latency, ttl, time_packet_sent))
+            return packets
 
 class packet():
     def __init__(self, latency = None, ttl = None, time_packet_sent = None, dropped = False):
