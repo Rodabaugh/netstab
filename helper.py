6def6172dc3008c60e41cfe498adeb7cced9526d
Rodabaugh <erik@erikrodabaugh.com>
2024-08-31 12:57:19 -0700

    Added support to ping 3 hosts at once

--- helper.py
+++ helper.py
@@ -1,9 +1,10 @@
 import time
 import datetime
+from threading import Thread
 
 def run_ping(host, number, tester):
     packets = list()
-    for x in range(number):
+    for _ in range(number):
         ping = tester.ping(host)
         packets.append(tester.parse(ping[0], ping[1], ping[2], host).pop())
         time.sleep(1)
@@ -14,6 +15,29 @@ def write_data(packets, log_file):
         local_time = datetime.datetime.fromtimestamp(packet.time_packet_sent).strftime('%Y-%m-%d %H:%M:%S')
         log_file.writelines(f"{local_time},{packet.host},{packet.latency},{packet.ttl},{packet.dropped}\n")
 
-def ping_and_write_log(host, number, tester, logfile):
-    packets = run_ping(host, number, tester)
+def ping_and_write_log(hosts, number, tester, logfile):
+    packets = list()
+    threads = list()
+    for host in hosts:
+        threads.append(ThreadWithReturnValue(target=run_ping, args=(host, number, tester)))
+    for thread in threads:
+        thread.start()
+    for thread in threads:
+        result = thread.join()
+        for packet in result:
+            packets.append(packet)
     write_data(packets, logfile)
+
+class ThreadWithReturnValue(Thread):
+    def __init__(self, group=None, target=None, name=None,
+                 args=(), kwargs={}, Verbose=None):
+        Thread.__init__(self, group, target, name, args, kwargs)
+        self._return = None
+
+    def run(self):
+        if self._target is not None:
+            self._return = self._target(*self._args,
+                                                **self._kwargs)
+    def join(self, *args):
+        Thread.join(self, *args)
+        return self._return
--- ui.py
+++ ui.py
@@ -13,50 +13,9 @@ class App(Tk):
         self.geometry(f"{width}x{height}")
         self.running = False
 
-        self.host_label = Label(self, text="Remote host:", font=(UI_FONT, UI_FONT_SIZE))
-        self.host_label.grid(row=0, column=0, padx=UI_X_PADDING, pady=UI_Y_PADDING)
-
-        self.host_entry = Entry(self, width=20, font=(UI_FONT, UI_FONT_SIZE))
-        self.host_entry.insert(0, DEFAULT_HOST)
-        self.host_entry.grid(row=0, column=1, padx=UI_X_PADDING, pady=UI_Y_PADDING)
-
-        self.num_packets_label = Label(self, text="Number of Packets:", font=(UI_FONT, UI_FONT_SIZE))
-        self.num_packets_label.grid(row=1, column=0, padx=UI_X_PADDING, pady=UI_Y_PADDING)
-
-        self.num_packets_entry = Entry(self, width=20, font=(UI_FONT, UI_FONT_SIZE))
-        self.num_packets_entry.insert(0, DEFAULT_NUM_PACKETS)
-        self.num_packets_entry.grid(row=1, column=1, padx=UI_X_PADDING, pady=UI_Y_PADDING)
-
-        self.ping_button = Button(self, text="Ping!", command=self.ping_button_command)
-        self.ping_button.grid(row=3, column=0, padx=UI_X_PADDING, pady=UI_Y_PADDING)
-        self.stop_button = Button(text="Stop!", command=self.stop_button_command)
-        self.stop_button.grid(row=3, column=1, padx=UI_X_PADDING, pady=UI_Y_PADDING)
-
-        self.status_label = Label(self, text=STATUS_NOT_PINGING_TEXT, font=(UI_FONT, UI_FONT_SIZE))
-        self.status_label.grid(row=4, column=0, padx=UI_X_PADDING, pady=UI_Y_PADDING)
+        Ping_frame(self)
 
         self.protocol("WM_DELETE_WINDOW", self.close)
-        #app_frame(self)
-    
-    def change(self):
-        self.host_label.config(text="byeee!")
-
-    def ping_button_command(self):
-        self.status_label.config(text=STATUS_PINGING_TEXT)
-        if int(self.num_packets_entry.get()) == 0:
-            self.infinite_ping = True
-            while self.infinite_ping == True:
-                self.redraw()
-                ping_and_write_log(self.host_entry.get(), 1, self.tester, self.log_file)
-        else:
-            self.redraw()
-            ping_and_write_log(self.host_entry.get(), int(self.num_packets_entry.get()), self.tester, self.log_file)
-        self.status_label.config(text=STATUS_NOT_PINGING_TEXT)
-
-    def stop_button_command(self):
-        self.infinite_ping = False
-        self.status_label.config(text=STATUS_STOP_PINGING_TEXT)
-        self.redraw()
 
     def redraw(self):
         self.update_idletasks()
@@ -71,17 +30,77 @@ class App(Tk):
     def close(self):
         self.running = False
 
-class app_frame(Frame):
+class Ping_frame(Frame):
     def __init__(self, parent):
         super().__init__(parent)
+        self.parent = parent
+        self.pack(pady=UI_Y_PADDING)
 
-        self.pack(pady=20)
+        self.host1_label = Label(self, text="Remote host:", font=(UI_FONT, UI_FONT_SIZE))
+        self.host1_label.grid(row=0, column=0, padx=UI_X_PADDING)
+        self.host1_entry = Entry(self, width=20, font=(UI_FONT, UI_FONT_SIZE))
+        self.host1_entry.insert(0, DEFAULT_HOST)
+        self.host1_entry.grid(row=0, column=1, padx=UI_X_PADDING)
 
-        self.button_1 = Button(self, text="Change Button 1")
-        self.button_2 = Button(self, text="Change Button 2")
-        self.button_3 = Button(self, text="Change Button 3")
+        self.host2_label = Label(self, text="Remote host:", font=(UI_FONT, UI_FONT_SIZE))
+        self.host2_label.grid(row=1, column=0, padx=UI_X_PADDING, pady=UI_Y_PADDING)
+        self.host2_entry = Entry(self, width=20, font=(UI_FONT, UI_FONT_SIZE))
+        self.host2_entry.grid(row=1, column=1, padx=UI_X_PADDING, pady=UI_Y_PADDING)
 
+        self.host3_label = Label(self, text="Remote host:", font=(UI_FONT, UI_FONT_SIZE))
+        self.host3_label.grid(row=2, column=0, padx=UI_X_PADDING, pady=UI_Y_PADDING)
+        self.host3_entry = Entry(self, width=20, font=(UI_FONT, UI_FONT_SIZE))
+        self.host3_entry.grid(row=2, column=1, padx=UI_X_PADDING, pady=UI_Y_PADDING)
 
-        self.button_1.grid(row=0, column=0, padx=0)
-        self.button_2.grid(row=0, column=1, padx=0)
-        self.button_3.grid(row=0, column=2, padx=100)
+        self.num_packets_label = Label(self, text="Number of Packets:", font=(UI_FONT, UI_FONT_SIZE))
+        self.num_packets_label.grid(row=3, column=0, padx=UI_X_PADDING, pady=UI_Y_PADDING)
+        self.num_packets_entry = Entry(self, width=20, font=(UI_FONT, UI_FONT_SIZE))
+        self.num_packets_entry.insert(0, DEFAULT_NUM_PACKETS)
+        self.num_packets_entry.grid(row=3, column=1, padx=UI_X_PADDING, pady=UI_Y_PADDING)
+
+        self.ping_button = Button(self, text="Ping!", command=self.ping_button_command)
+        self.ping_button.grid(row=4, column=0, padx=UI_X_PADDING, pady=UI_Y_PADDING)
+        self.stop_button = Button(self, text="Stop!", command=self.stop_button_command)
+        self.stop_button.grid(row=4, column=1, padx=UI_X_PADDING, pady=UI_Y_PADDING)
+
+        self.status_label = Label(self, text=STATUS_NOT_PINGING_TEXT, font=(UI_FONT, UI_FONT_SIZE))
+        self.status_label.grid(row=5, column=0, padx=UI_X_PADDING, pady=UI_Y_PADDING)
+
+    def ping_button_command(self):
+        hosts = self.get_hosts()
+        if hosts == None:
+            self.status_label.config(text=STATUS_NO_HOSTS)
+            return
+        self.status_label.config(text=STATUS_PINGING_TEXT)
+
+        # Preform the pinging
+        if int(self.num_packets_entry.get()) == 0:
+            self.infinite_ping = True
+            while self.infinite_ping == True:
+                self.parent.redraw()
+                ping_and_write_log(hosts, 1, self.parent.tester, self.parent.log_file)
+        else:
+            for i in range(int(self.num_packets_entry.get())):
+                self.parent.redraw()
+                ping_and_write_log(hosts, 1, self.parent.tester, self.parent.log_file)
+        self.status_label.config(text=STATUS_NOT_PINGING_TEXT)
+
+    def stop_button_command(self):
+        self.infinite_ping = False
+        self.status_label.config(text=STATUS_STOP_PINGING_TEXT)
+        self.parent.redraw()
+
+    def get_hosts(self):
+        hosts = list()
+
+        if self.host1_entry.get() != "":
+            hosts.append(self.host1_entry.get())
+        if self.host2_entry.get() != "":
+            hosts.append(self.host2_entry.get())
+        if self.host3_entry.get() != "":
+            hosts.append(self.host3_entry.get())
+
+        if len(hosts) == 0:
+            return None
+        else:
+            return hosts
